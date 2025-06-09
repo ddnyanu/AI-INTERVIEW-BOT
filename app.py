fa8b55b1326f970318c9e785843691807f7b9d57
sarthak kardile <dhotrednyanu@gmail.com>
2025-06-09 15:26:48 +0530

    change user name to candiate name

--- app.py
+++ app.py
@@ -728,7 +728,7 @@ def home():
     session['interview_data'] = init_interview_data()
         # Provide fallback if data not available
     data = {
-        "user_name": "Guest",
+        "candidate_name": "Guest",
         "email": "",
         "match_score": "",
         "jd_text": "",
@@ -758,8 +758,8 @@ def start_interview():
     logger.debug(f"Received resume text (first 300 chars): {resume_text[:300]}")
     logger.debug(f"Received JD text (first 300 chars): {jd_text[:300]}")
 
-    user_name = data.get('fileName', 'Candidate')  # match the key sent from JS
-    candidate_name = user_name.split('.')[0].replace('_', ' ').replace('-', ' ')
+    candidate_name = data.get('fileName', 'Candidate')  # match the key sent from JS
+    candidate_name = candidate_name.split('.')[0].replace('_', ' ').replace('-', ' ')
     print("Candidate Name:----------------------------------------------------------", candidate_name)
     
     # Initialize interview session
@@ -782,7 +782,7 @@ def start_interview():
     interview_data['years_experience'] = int(session.get('years_experience', 0))
     interview_data['resume'] = session.get('resume_text')
     interview_data['jd'] = session.get('jd_text')
-    interview_data['user_name'] = session.get('user_name', 'Anonymous')
+    interview_data['candidate_name'] = session.get('candidate_name', 'Anonymous')
     interview_data['email'] = session.get('email')
 
     # Timestamps
@@ -1277,7 +1277,7 @@ def reset_interview():
 from datetime import datetime, timezone
 
 def create_text_report_from_interview_data(interview_data):
-    candidate = interview_data.get('user_name', 'Unknown Candidate')
+    candidate = interview_data.get('candidate_name', 'Unknown Candidate')
     role = interview_data.get('role', 'Unknown Role')
     exp_level = interview_data.get('experience_level', 'Unknown')
     years = interview_data.get('years_experience', 0)
@@ -1332,7 +1332,7 @@ def save_report_to_django(interview_data):
     report_txt = create_text_report_from_interview_data(interview_data)
 
     payload = {
-        "user_name": interview_data.get("user_name"),
+        "candidate_name": interview_data.get("candidate_name"),
         "role": interview_data.get("role"),
         "organization_name": interview_data.get("organization_name", "N/A"),
         "experience_level": interview_data.get("experience_level"),
